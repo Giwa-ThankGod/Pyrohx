020bb8c24d9c28f3ff416b1c95940de7d15b92a1
Giwa-ThankGod <giwathankgod101@gmail.com>
2026-02-11 22:39:14 +0100

    feat: Update main view
    - Add mobile responsiveness
    - Add refs to manage header navigation
    - Pass new prop values to updated cards
    - Fix duplicate key error

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,14 @@ import TestimonyCard from './components/ui/testimony-card'
 
 import ArrowLeft from './assets/icons/arrow-left-1.svg?react';
 import ArrowRight from './assets/icons/arrow-right-1.svg?react';
+import { scrollToSection } from './utils/generic'
 
 function App() {
-  const testimonialRef = useRef(null);
+  const servicesRef = useRef(null);
+  const whyRef = useRef(null);
+  const contactRef = useRef(null);
 
+  const testimonialRef = useRef(null);
   const scrollTestimonials = (direction) => {
     const container = testimonialRef.current;
     if (!container) return;
@@ -44,51 +48,56 @@ function App() {
         </div>
 
         <div className='z-10'>
-          <Header />
-        <div className='flex items-center justify-center py-36 sora text-3xl md:text-6xl'>
-          <div className='w-full md:w-1/2 space-y-3 text-center'>
+          <Header 
+            servicesRef={servicesRef}
+            whyRef={whyRef}
+            contactRef={contactRef}
+          />
 
-            <div className='flex items-center justify-center gap-5'>
-              <div className="relative w-[55px] h-[26px]">
-                <div className="absolute w-7 h-7 border-2 border-white rounded-full bg-red-500 left-0 top-0"></div>
-                <div className="absolute w-7 h-7 border-2 border-white rounded-full bg-green-500 left-[18px] top-0"></div>
-                <div className="absolute w-7 h-7 border-2 border-white rounded-full bg-blue-500 left-[36px] top-0"></div>
+          <div className='flex items-center justify-center py-36 sora text-3xl md:text-6xl'>
+            <div className='w-full md:w-8/12 lg:w-1/2 space-y-3 text-center'>
+
+              <div className='flex items-center justify-center gap-5'>
+                <div className="relative w-[55px] h-[26px]">
+                  <div className="absolute w-7 h-7 border-2 border-white rounded-full bg-red-500 left-0 top-0"></div>
+                  <div className="absolute w-7 h-7 border-2 border-white rounded-full bg-green-500 left-[18px] top-0"></div>
+                  <div className="absolute w-7 h-7 border-2 border-white rounded-full bg-blue-500 left-[36px] top-0"></div>
+                </div>
+                <p className='text-sm md:text-base'>Trusted by 5+ happy clients</p>
               </div>
-              <p className='text-sm md:text-base'>Trusted by 5+ happy clients</p>
-            </div>
 
-            <h2>Build a Digital Asset That</h2>
-            <div className='flex items-center justify-center gap-2 md:gap-5'>
-              <h2>Drives</h2>
-              <div className='relative bg-[rgba(65,184,131,0.1)]'>
-                <span className='absolute -top-1 -left-1 w-3 h-3 bg-white border-primary'></span>
-                <span className='absolute -top-1 -right-1 w-3 h-3 bg-white border-primary'></span>
-                <div contentEditable className="p-3 border border-primary font-semibold text-3xl md:text-6xl
+              <h2>Build a Digital Asset That</h2>
+              <div className='flex items-center justify-center gap-2 md:gap-5'>
+                <h2>Drives</h2>
+                <div className='relative bg-[rgba(65,184,131,0.1)]'>
+                  <span className='absolute -top-1 -left-1 w-3 h-3 bg-white border-primary'></span>
+                  <span className='absolute -top-1 -right-1 w-3 h-3 bg-white border-primary'></span>
+                  <div className="p-3 border border-primary font-semibold text-3xl md:text-6xl
                   bg-gradient-to-b from-[#00CA99] to-[#0CDEAB] 
                   bg-clip-text text-transparent">
-                  Real Growth
-                </div>
+                    Real Growth
+                  </div>
 
-                <span className='absolute -bottom-1 -left-1 w-3 h-3 bg-white border-primary'></span>
-                <span className='absolute -bottom-1 -right-1 w-3 h-3 bg-white border-primary'></span>
+                  <span className='absolute -bottom-1 -left-1 w-3 h-3 bg-white border-primary'></span>
+                  <span className='absolute -bottom-1 -right-1 w-3 h-3 bg-white border-primary'></span>
+                </div>
+              </div>
+
+              <p className='my-5 px-5 md:px-20 text-sm md:text-base text-tertiary'>
+                We design and deliver websites, integrations, and AI-powered solutions that help your business grow faster and operate smarter.
+              </p>
+
+              {/* Action Buttons */}
+              <div className="flex flex-wrap items-center justify-center gap-3 px-5 md:px-0 text-sm md:text-base">
+                <button className="w-full md:w-auto px-6 h-12 flex items-center justify-center gap-2 glow-button text-white rounded-full cursor-pointer">
+                  Schedule a Call
+                </button>
+                <button onClick={() => scrollToSection(servicesRef)} className="w-full md:w-auto px-6 h-12 flex items-center justify-center gap-2 border border-[var(--primary-color)] text-[var(--primary-color)] rounded-full cursor-pointer">
+                  Explore Services
+                </button>
               </div>
             </div>
-
-            <p className='my-5 px-5 md:px-20 text-sm md:text-base text-tertiary'>
-              We design and deliver websites, integrations, and AI-powered solutions that help your business grow faster and operate smarter.
-            </p>
-
-            {/* Action Buttons */}
-            <div className="flex items-center justify-center gap-3 text-sm md:text-base">
-              <button className="px-6 h-12 flex items-center gap-2 glow-button text-white rounded-full cursor-pointer">
-                Schedule a Call
-              </button>
-              <button className="px-6 h-12 flex items-center gap-2 border border-[var(--primary-color)] text-[var(--primary-color)] rounded-full cursor-pointer">
-                Explore services
-              </button>
-            </div>
           </div>
-        </div>
         </div>
       </div>
 
@@ -111,6 +120,7 @@ function App() {
       </div>
 
       {/* Services */}
+      <div ref={servicesRef}></div>
       <section className='px-5 md:px-24 mt-10 md:mt-28 space-y-3'>
         <h2 className='sora text-xl md:text-4xl'>Our Services</h2>
         <p className="w-full md:w-4/12 md:pr-8 text-sm md:text-base text-tertiary">
@@ -130,6 +140,7 @@ function App() {
       </section>
 
       {/* Why Pyrohx */}
+      <div ref={whyRef}></div>
       <section className='px-5 md:px-24 mt-10 md:mt-28 space-y-3'>
         <h2 className='sora text-xl md:text-4xl'>Why Choose Pyrohx</h2>
         <p className="w-full md:w-4/12 md:pr-8 text-sm md:text-base text-tertiary">
@@ -149,8 +160,8 @@ function App() {
       </section>
 
       {/* About Us */}
-      <section className='mt-10 md:mt-28 bg-[#FAFAFA]'>
-        <div className='flex flex-col items-center justify-center space-y-3 mt-5 md:pt-24 textx-center'>
+      <section className='px-5 md:px-0 mt-10 md:mt-28 bg-[#FAFAFA]'>
+        <div className='flex flex-col items-center justify-center space-y-3 mt-5 md:pt-24 text-center'>
           <h2 className='sora text-xl md:text-4xl'>How We Work</h2>
           <p className="w-full md:w-4/12 md:pr-8 text-sm md:text-base text-tertiary text-center">
             A clear process that keeps your project organized, fast, and stress-free, from first call to launch.
@@ -171,12 +182,12 @@ function App() {
         </div>
       </section>
 
-      <section className='relative bg-[var(--secondary-color)] px-5 md:p-24 mt-10 md:mt-28 space-y-3'>
+      <section className='relative bg-[var(--secondary-color)] md:p-24 mt-10 md:mt-28 space-y-3'>
         <img src={BackgroundImage} className="w-full h-full absolute inset-0 z-0" />
 
         <div className="
-          absolute bottom-0 -left-50
-          w-8/12 h-[130vh]
+          absolute bottom-0 md:-left-50
+          md:w-8/12 md:h-[130vh]
           rounded-full
           border-[70px] border-[var(--primary-color)]
           blur-2xl
@@ -190,7 +201,7 @@ function App() {
           <img src={UnionBg} alt="" className='' />
         </div>
 
-        <div className='relative z-10 p-5 md:p-0'>
+        <div className='relative z-10 mt-10 p-5'>
           <h2 className='sora text-xl md:text-4xl text-white'>Testimonials</h2>
           <p className="w-full md:w-4/12 text-sm md:text-base text-[#D3D3D3]">
             From startups to service brands, clients choose Pyrohx for clean execution, clear communication, and results that last.
@@ -204,8 +215,10 @@ function App() {
             {dataModel.testimonials.map((item, index) => (
               <div key={item.name} className="flex-shrink-0">
                 <TestimonyCard
+                  key={item.name + index}
                   name={item.name}
                   feedback={item.feedback}
+                  photo={item.photo}
                 />
               </div>
             ))}
@@ -232,6 +245,8 @@ function App() {
             </div>
           </div>
 
+          {/* Contact Us */}
+          <div ref={contactRef}></div>
           <div className='w-full flex flex-col md:flex-row gap-5 my-24 z-10'>
             <div className='md:w-7/12'>
               <h2 className='sora text-xl md:text-4xl text-white'>Ready to build <br /> something better?</h2>
@@ -265,10 +280,10 @@ function App() {
 
                 {/* Action Buttons */}
                 <div className="flex flex-wrap items-center gap-3 text-sm md:text-base">
-                  <button className="px-6 h-12 flex items-center gap-2 text-white bg-[var(--primary-color)] border border-gray-500 rounded-full cursor-pointer">
+                  <button className="w-full md:w-auto px-6 h-12 flex items-center justify-center gap-2 text-white bg-[var(--primary-color)] border border-gray-500 rounded-full cursor-pointer">
                     Send a message
                   </button>
-                  <button className="px-6 h-12 flex items-center gap-2 text-[var(--primary-color)] border border-[var(--primary-color)] rounded-full cursor-pointer">
+                  <button className="w-full md:w-auto px-6 h-12 flex items-center justify-center gap-2 text-[var(--primary-color)] border border-[var(--primary-color)] rounded-full cursor-pointer">
                     Schedule a Call
                   </button>
                 </div>
