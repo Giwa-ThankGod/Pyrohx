0dc230d6b75fa41a7e3bded0fbcae858fda82e01
Giwa-ThankGod <giwathankgod101@gmail.com>
2026-02-22 22:03:03 +0100

    refactor: Adjust UI for how-card.jsx

--- src/components/ui/how-card.jsx
+++ src/components/ui/how-card.jsx
@@ -2,7 +2,7 @@ import Phone from "../../assets/icons/phone.svg?react";
 import Map from "../../assets/icons/map.svg?react";
 import Connection from "../../assets/icons/connection.svg?react";
 import Rocket from "../../assets/icons/rocket.svg?react";
-import ArrowRight from "../../assets/icons/arrow-right.svg?react";
+import ArrowRight from "../../assets/icons/arrow-right-2.svg?react";
 
 const IconMapper = {
     phone: <Phone className="w-14 h-14 ml-8" />,
@@ -20,7 +20,8 @@ const HowCard = ({ icon, index, title, description, stackIndex }) => {
         transform: `translateY(${stackIndex * 0}px)`,
       }}
     >
-      <div className="flex flex-col md:flex-row items-center justify-between py-10 px-5 md:px-10 lg:px-24 bg-[#FAFAFA] border-b-2 border-[var(--tertiary-color)]">
+      <div className={`flex flex-col md:flex-row items-center justify-between py-10 px-5 
+        md:px-10 lg:px-24 bg-[#FAFAFA] ${index !== "04" ? 'border-b-2 border-[var(--tertiary-color)]' : ''}`}>
         <div className="w-full flex flex-col md:flex-row items-center md:gap-3 lg:gap-10">
           <h2 className="text-4xl lg:text-6xl text-[var(--tertiary-color)] font-medium">
             {index}
@@ -32,7 +33,7 @@ const HowCard = ({ icon, index, title, description, stackIndex }) => {
             </div>
 
             <div className="flex items-center gap-3 lg:gap-8">
-              <h3 className="sora text-2xl lg:text-4xl">{title}</h3>
+              <h3 className="sora text-2xl md:text-3xl lg:text-5xl">{title}</h3>
               <ArrowRight className="hidden md:block" />
             </div>
           </div>
